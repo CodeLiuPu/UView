6cc181ea762ef30470ba72ed565385ec570b740f
liupu <575866447@qq.com>
2019-11-05 16:58:51 +0800

    PorterDuffColorFilter

--- app/src/main/java/com/update/uview/study/colorfilter/ColorFilterView.java
+++ app/src/main/java/com/update/uview/study/colorfilter/ColorFilterView.java
@@ -4,9 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.LightingColorFilter;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,7 +22,6 @@ import com.update.uview.R;
 public class ColorFilterView extends View {
     private Paint mPaint;
     private Bitmap mBitmap;
-    private ColorMatrixColorFilter mColorMatrixColorFilter;
 
     public ColorFilterView(Context context) {
         this(context, null);
@@ -45,6 +45,29 @@ public class ColorFilterView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+
+// ------------------- LightingColorFilter ------------------------
+//        // 原图
+//        LightingColorFilter lightingColorFilter = new LightingColorFilter(0xffffff,0x000000);
+//        // 过滤红色
+////        LightingColorFilter lightingColorFilter = new LightingColorFilter(0x00ffff,0x000000);
+//        // 绿色更亮
+////        LightingColorFilter lightingColorFilter = new LightingColorFilter(0xffffff,0x003000);
+//
+//        mPaint.setColorFilter(lightingColorFilter);
+//        canvas.drawBitmap(mBitmap,100,100,mPaint);
+// ---------------------------------------------------------------
+
+
+// ------------------- PorterDuffColorFilter ------------------------
+        PorterDuffColorFilter porterDuffColorFilter = new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.ADD);
+        mPaint.setColorFilter(porterDuffColorFilter);
+        canvas.drawBitmap(mBitmap, 100, 100, mPaint);
+// ---------------------------------------------------------------
+
+
+// ------------------- ColorMatrixColorFilter ------------------------
+
 //        ColorMatrix cm = new ColorMatrix();
 //        //亮度调节
 //        cm.setScale(1, 2, 1, 1);
@@ -55,15 +78,11 @@ public class ColorFilterView extends View {
 //        //色调调节
 //        cm.setRotate(0, 45);
 //
-//        mColorMatrixColorFilter = new ColorMatrixColorFilter(cm);
-//        mPaint.setColorFilter(mColorMatrixColorFilter);
+//        ColorMatrixColorFilter colorMatrixColorFilter = new ColorMatrixColorFilter(cm);
+//        mPaint.setColorFilter(colorMatrixColorFilter);
 //
 //        canvas.drawBitmap(mBitmap, 100, 100, mPaint);
-
-
-        LightingColorFilter lightingColorFilter = new LightingColorFilter(0x0000ffff,0x00000000);
-        mPaint.setColorFilter(lightingColorFilter);
-        canvas.drawBitmap(mBitmap,100,100,mPaint);
+// ---------------------------------------------------------------
     }
 
 }
