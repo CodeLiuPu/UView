5a19d5e4de979943cfff711b029e6d81c928f4c0
liupu <575866447@qq.com>
2019-11-08 17:51:47 +0800

    PathView

--- app/src/main/java/com/update/uview/study/bezier/PathView.java
+++ app/src/main/java/com/update/uview/study/bezier/PathView.java
@@ -56,14 +56,39 @@ public class PathView extends View {
 //        mPath.addArc(100,100,200,200,0,200);
 
         // 添加 矩形
-        mPath.addRect(500, 500, 900, 900, Path.Direction.CCW);
+//        mPath.addRect(500, 500, 900, 900, Path.Direction.CCW);
 
 //        mPath.addCircle(0, 0, 10, Path.Direction.CW);
 
 
         // ------------------ 追加图形 ---------------------
         // xxxTo画线
-        mPath.arcTo(400, 200, 600, 400, -180, 225, false);
+//        mPath.arcTo(400, 200, 600, 400, -180, 225, false);
+
+        // 添加一个路径
+//        mPath.moveTo(100, 70);
+//        mPath.lineTo(140, 180);
+//        mPath.lineTo(250, 330);
+//        mPath.lineTo(400, 630);
+//        mPath.lineTo(100, 830);
+//
+//        Path newPath = new Path();
+//        newPath.moveTo(100, 1000);
+//        newPath.lineTo(600, 1300);
+//        newPath.lineTo(400, 1700);
+//        mPath.addPath(newPath);
+
+        //画二阶贝塞尔曲线
+//        mPath.moveTo(300, 500);
+//        mPath.quadTo(500, 100, 800, 500);
+        //参数表示相对位置，等同于上面一行代码
+//        mPath.rQuadTo(200, -400, 500, 0);
+
+        //画三阶贝塞尔曲线
+        mPath.moveTo(300, 500);
+//        mPath.cubicTo(500, 100, 600, 1200, 800, 500);
+        //参数表示相对位置，等同于上面一行代码
+        mPath.rCubicTo(200, -400, 300, 700, 500, 0);
 
         canvas.drawPath(mPath, mPaint);
     }
