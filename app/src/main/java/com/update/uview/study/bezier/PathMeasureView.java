670e3a9935f28fd1b7c1a5b3decafc6dd35d5c79
liupu <575866447@qq.com>
2019-11-13 16:03:48 +0800

    PathMeasureView

--- app/src/main/java/com/update/uview/study/bezier/PathMeasureView.java
+++ app/src/main/java/com/update/uview/study/bezier/PathMeasureView.java
@@ -19,6 +19,8 @@ import android.view.View;
 public class PathMeasureView extends View {
 
     private Paint mPaint;
+    private Paint mLinePaint; //坐标系
+
     private Path mPath;
 
     public PathMeasureView(Context context) {
@@ -36,9 +38,14 @@ public class PathMeasureView extends View {
 
     private void init() {
         mPaint = new Paint();
-        mPaint.setColor(Color.RED);
-        mPaint.setStrokeWidth(4);
         mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(4);
+
+        mLinePaint = new Paint();
+        mLinePaint.setStyle(Paint.Style.STROKE);
+        mLinePaint.setColor(Color.RED);
+        mLinePaint.setStrokeWidth(6);
 
         mPath = new Path();
     }
@@ -47,22 +54,36 @@ public class PathMeasureView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2, mLinePaint);
+        canvas.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight(), mLinePaint);
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+
         Path path = new Path();
         path.lineTo(0, 200);
         path.lineTo(200, 200);
         path.lineTo(200, 0);
 
-        canvas.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2, mPaint);
-        canvas.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight(), mPaint);
-        canvas.translate(getWidth() / 2, getHeight() / 2);
+//        /**
+//         * pathMeasure 需要关联一个 创建好的 path
+//         * forceClosed会影响Path的测量结果
+//         */
+//        PathMeasure pathMeasure = new PathMeasure();
+//        pathMeasure.setPath(path, false);
+//        log("onDraw: " + pathMeasure.getLength());
+//
+//        PathMeasure pathMeasure2 = new PathMeasure();
+//        pathMeasure2.setPath(path, true);
+//        log("onDraw2: " + pathMeasure2.getLength());
 
-        /**
-         * pathMeasure 需要关联一个 创建好的 path
-         * forceClosed会影响Path的测量结果
-         */
-        PathMeasure pathMeasure = new PathMeasure();
+        PathMeasure pathMeasure = new PathMeasure(path, false);
+        log("onDraw: " + pathMeasure.getLength());
+
+        path.lineTo(200, -200);
+        log("onDraw: " + pathMeasure.getLength());
+
+        // 如果 path 进行了调整, 需要重新调用 setPath 来进行关联
         pathMeasure.setPath(path, false);
-        log("onDraw: forceCloseed = false " + pathMeasure.getLength());
+        log("onDraw: " + pathMeasure.getLength());
 
         canvas.drawPath(path, mPaint);
     }
