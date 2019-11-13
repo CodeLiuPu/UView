9555cc22d4140331905a87eef1bcb21386af665d
liupu <575866447@qq.com>
2019-11-13 18:00:40 +0800

    Activity

---
+++ app/src/main/java/com/update/uview/hand_write_touch/Activity.java
@@ -0,0 +1,10 @@
+package com.update.uview.hand_write_touch;
+
+/**
+ * @author : liupu
+ * date   : 2019/11/13
+ * desc   :
+ * github : https://github.com/CodeLiuPu/
+ */
+public class Activity {
+}
