1a89b2e122bf46de6e055b592fa47cc3611f040c
liupu <575866447@qq.com>
2019-11-14 11:20:05 +0800

    View

---
+++ app/src/main/java/com/update/uview/hand_write_touch/MotionEvent.java
@@ -0,0 +1,10 @@
+package com.update.uview.hand_write_touch;
+
+/**
+ * @author : liupu
+ * date   : 2019/11/14
+ * desc   :
+ * github : https://github.com/CodeLiuPu/
+ */
+public class MotionEvent {
+}
---
+++ app/src/main/java/com/update/uview/hand_write_touch/listener/OnTouchListener.java
@@ -0,0 +1,11 @@
+package com.update.uview.hand_write_touch.listener;
+
+/**
+ * @author : liupu
+ * date   : 2019/11/14
+ * desc   :
+ * github : https://github.com/CodeLiuPu/
+ */
+public interface OnTouchListener {
+    void onTouch();
+}
