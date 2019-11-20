2cb34fc20a4210568c7966fb534988a17d22cc93
liupu <575866447@qq.com>
2019-11-20 14:46:13 +0800

    TouchTarget

--- app/src/main/java/com/update/uview/hand_write_touch/ViewGroup.java
+++ app/src/main/java/com/update/uview/hand_write_touch/ViewGroup.java
@@ -31,6 +31,64 @@ public class ViewGroup extends View {
         mChildrens = childList.toArray(new View[childList.size()]);
     }
 
+    // 事件分发入口
+    public boolean dispatchTouchEvent(){
+
+
+        
+
+
+
+        return false;
+    }
+
+
+    // 拦截事件
+    public boolean onInterceptTouchEvent(MotionEvent event){
+        return false;
+    }
+
+
     private static final class TouchTarget {
+        // 当前缓存的View
+        private View child;
+        // 回收池
+        private static TouchTarget sRecycleBin;
+        // size
+        private static int sRecycleCount;
+        private static final Object sRecycleLock = new Object[0];
+        public TouchTarget next;
+
+        private TouchTarget() {
+        }
+
+        public static TouchTarget obtain(View child) {
+            TouchTarget target;
+            synchronized (sRecycleLock) {
+                if (sRecycleBin == null) {
+                    target = new TouchTarget();
+                } else {
+                    target = sRecycleBin;
+                }
+                sRecycleBin = target.next;
+                sRecycleCount++;
+                target.next = null;
+            }
+            target.child = child;
+            return target;
+        }
+
+        public void recycle(){
+            if (child ==null){
+                throw new IllegalStateException("已经被回收过了");
+            }
+            synchronized (sRecycleLock){
+                if (sRecycleCount < 32){
+                    next = sRecycleBin;
+                    sRecycleBin = this;
+                    sRecycleCount++;
+                }
+            }
+        }
     }
 }
