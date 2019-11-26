b05966784fc26619053942e05715f8c1c7aca440
liupu <575866447@qq.com>
2019-11-26 23:35:21 +0800

    LayoutInflaterActivity

--- app/src/main/java/com/update/uview/layout_inflate/LayoutInflaterActivity.java
+++ app/src/main/java/com/update/uview/layout_inflate/LayoutInflaterActivity.java
@@ -3,24 +3,17 @@ package com.update.uview.layout_inflate;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.LayoutInflaterCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.update.uview.R;
-import com.update.uview.adapter.FmPagerAdapter;
 import com.update.uview.choreographer.ChoreographerFragment;
-import com.update.uview.layout.flow_layout.FlowLayoutFragment;
-import com.update.uview.layout.triangle_layout.TriangleLayoutFragment;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author : liupu
@@ -33,22 +26,18 @@ public class LayoutInflaterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         LayoutInflaterCompat.setFactory2(getLayoutInflater(), new LayoutInflater.Factory2() {
-
             @Override
             public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
                 // 对View执行一些 Hook 逻辑
                 /**
-                 * 1. 使用场景 1
-                 * 替换 TextView 为 自定义的TextView
-                 * if("TextView".equals(name)){
-                 *      // 创建自定义TextView
-                 * }
-                 * 并 return 回去
+                 * 使用场景 2
+                 * 计算 View 创建消耗的时间
                  */
-
-                return null;
+                long startTime = System.currentTimeMillis();
+                View view = getDelegate().createView(parent, name, context, attrs);
+                Log.e("update", "viewName " + name + " costTime " + (System.currentTimeMillis() - startTime));
+                return view;
             }
 
             @Override
@@ -56,15 +45,6 @@ public class LayoutInflaterActivity extends AppCompatActivity {
                 return null;
             }
         });
-
-
-
-
-
-
-
-
-
 
 
         super.onCreate(savedInstanceState);
