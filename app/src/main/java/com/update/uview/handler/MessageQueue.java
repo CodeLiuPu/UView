827c116a75ed78d4edd199340c9038dbb0cbc826
liupu <575866447@qq.com>
2019-11-28 21:39:18 +0800

    MessageQueue

--- app/src/main/java/com/update/uview/handler/MessageQueue.java
+++ app/src/main/java/com/update/uview/handler/MessageQueue.java
@@ -1,5 +1,8 @@
 package com.update.uview.handler;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * @author : liupu
  * date   : 2019/11/28
@@ -7,4 +10,28 @@ package com.update.uview.handler;
  * github : https://github.com/CodeLiuPu/
  */
 public class MessageQueue {
+    
+    BlockingQueue<Message> blockingQueue;
+
+    public MessageQueue() {
+        blockingQueue = new ArrayBlockingQueue<>(50);
+    }
+
+    public void enqueueMessage(Message message) {
+        try {
+            blockingQueue.put(message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Message next() {
+        try {
+            return blockingQueue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
